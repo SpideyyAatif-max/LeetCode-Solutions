46a0a3b6e9a8e33a6a5330fbcd2b10a690e48b02
Aatif. <aatif.fast@gmail.com>
2026-07-22 00:35:55 +0500

    Time: 0 ms (100%), Space: 51.4 MB (41.73%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        int k=0;
-        for(int i=0;i<nums.Length-1;i++)
+        int k=1;
+        for(int i=1;i<nums.Length;i++)
         {
-            if(nums[i] != nums[i+1])
+            if(nums[i] != nums[i-1])
             {
                 nums[k] = nums[i];
                 k++;
             }
         }
-        nums[k] = nums[nums.Length-1];
-        k++;
         return k;
     }
 }
