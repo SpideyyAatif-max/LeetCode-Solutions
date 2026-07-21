fb729cec18338eb484b87b43ff4305657506ccc6
Aatif. <aatif.fast@gmail.com>
2026-07-22 00:27:39 +0500

    Time: 1 ms (91.02%), Space: 43.3 MB (49.24%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        for(int i=0;i<strs[0].Length;i++)
+        {
+            char curr = strs[0][i];
+            for(int j=0;j<strs.Length;j++)
+            {
+                if(i==strs[j].Length || strs[j][i]!=curr)
+                {
+                    return strs[0].Substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
