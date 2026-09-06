975066066c2ca7c6869ef5bf7b1c99e118e5de97
Aatif. <aatif.fast@gmail.com>
2026-09-06 23:43:16 +0500

    Time: 0 ms (100%), Space: 39.7 MB (76.34%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.Length - 1;
+        // Step 1: Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+        // Step 2: Count characters of the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
+}
